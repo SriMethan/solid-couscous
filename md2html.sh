5ed29d232e7846172310dea03ae8557f43204401
Methan Sritharan <methanroyal@gmail.com>
2023-06-13 07:39:53 +0530

    Add files via upload

---
+++ md2html.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Learn docs
+cd static/docs
+
+export PATH="../../node_modules/.bin/:$PATH"
+
+for f in *.md; do
+	showdown makehtml -i "$f" -o "$(basename -- "$f" .md).html" --flavor github
+	for lang in es hu it pt fr zh_CN zh_TW; do
+		if [ -e "$lang/$f" ]; then
+			showdown makehtml -i "$lang/$f" -o "$(basename -- "$f" .md).$lang.html" --flavor github
+		else
+			showdown makehtml -i "$f" -o "$(basename -- "$f" .md).$lang.html" --flavor github
+		fi
+	done
+done
+
+
+SRC='https://github.com/SriMethan/liantichess/blob/master'; 
+#DST='https://liantichess.herokuapp.com';
+DST='https://cdn.jsdelivr.net/gh/gbtami/pychess-variants\@1.7.7';
+find . -type f -name "*.html" -exec perl -pi -e s,$SRC,$DST,g '{}' +
+
+mkdir -p ../../templates/docs
+mv *.html ../../templates/docs
+
+# News
+cd ../news
+
+for f in *.md; do
+	showdown makehtml -i "$f" -o "$(basename -- "$f" .md).html" --flavor github
+	for lang in es hu it pt fr zh_CN zh_TW; do
+		if [ -e "$lang/$f" ]; then
+			showdown makehtml -i "$lang/$f" -o "$(basename -- "$f" .md).$lang.html" --flavor github
+		else
+			showdown makehtml -i "$f" -o "$(basename -- "$f" .md).$lang.html" --flavor github
+		fi
+	done
+done
+
+find . -type f -name "*.html" -exec perl -pi -e s,$SRC,$DST,g '{}' +
+
+mkdir -p ../../templates/news
+mv *.html ../../templates/news
